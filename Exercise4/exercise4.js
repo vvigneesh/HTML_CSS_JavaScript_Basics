83e9eea80634c5291394fb020bf5ad7f6f180ece
vvigneesh <89182069+vvigneesh@users.noreply.github.com>
2021-08-26 17:22:30 +0530

    Create exercise4.js

---
+++ Exercise4/exercise4.js
@@ -0,0 +1,51 @@
+const timer = document.getElementById('clock');
+var min=0;
+var sec=0;
+var stoptime=true;
+//var preDefined=document.getElementById('time-default').value
+//document.getElementById('clock').innerHTML=preDefined
+//document.getElementById("clock1").innerHTML = "00:00";
+
+function search(ele) {
+    
+    if (ele.code === "Enter") {
+        //alert('Hi');
+        startTimer()        
+    }
+}
+
+function startTimer(){
+    if (stoptime==true){
+        stoptime=false;
+        timerCycle();
+    }
+}
+
+function timerCycle(){
+    if (stoptime==false){
+        sec=parseInt(sec);
+        min=parseInt(min);
+        preDefined=parseInt(document.getElementById('time-default').value)
+        document.getElementById('time-default').style.display="none";
+        sec=sec+1;
+        //preDefined=parseInt(a);
+        //console.log(preDefined)
+
+        if (sec==60)
+        {
+            min=min+1;
+            sec=0;
+        }
+        if(sec<10 || sec==0){
+            sec='0'+sec;
+        }
+        if (min<10 || min==0){
+            min='0'+min;
+        }
+        if (min>=preDefined){
+            document.getElementById('clock').style.backgroundColor="red";
+        }
+        timer.innerHTML = min+":"+sec;
+        setTimeout("timerCycle()",1000);
+    }
+}
